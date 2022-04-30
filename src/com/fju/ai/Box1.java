9c1361ed0d130ea20f9520391b23b8443c090f46
andylin9611052 <andylin9611052@gmail.com>
2022-04-30 22:25:16 +0800

    examing

--- src/com/fju/ai/Box1.java
+++ src/com/fju/ai/Box1.java
@@ -2,6 +2,10 @@ package com.fju.ai;
 
 public class Box1 extends Box{
     public Box1() {
-        super(10,10,10);
+        super(10, 10, 10);
+    }
+    @Override
+    public String toString() {
+        return "box 1";
     }
 }
